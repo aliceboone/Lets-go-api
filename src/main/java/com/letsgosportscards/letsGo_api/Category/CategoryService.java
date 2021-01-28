ad4f095efd2a6c22919e2ab477cbd73191d3ee39
Alice Boone <alicegbone@gmail.com>
2021-01-28 14:54:55 -0800

    Create category service

--- src/main/java/com/letsgosportscards/letsGo_api/Category/CategoryService.java
+++ src/main/java/com/letsgosportscards/letsGo_api/Category/CategoryService.java
@@ -1,2 +1,20 @@
-package com.letsgosportscards.letsGo_api.Category;public class CategoryService {
+package com.letsgosportscards.letsGo_api.Category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getCategories() {
+        return categoryRepository.findAll();
+    }
 }
