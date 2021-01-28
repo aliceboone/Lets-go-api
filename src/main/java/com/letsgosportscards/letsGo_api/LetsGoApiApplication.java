6e8e60dd778ea1c9a1d48beb1ade706a17945b06
Alice Boone <alicegbone@gmail.com>
2021-01-28 11:03:57 -0800

    Spring initializer

---
+++ src/main/java/com/letsgosportscards/letsGo_api/LetsGoApiApplication.java
@@ -0,0 +1,13 @@
+package com.letsgosportscards.letsGo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsGoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsGoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letsgosportscards/letsGo_api/LetsGoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letsgosportscards.letsGo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsGoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
