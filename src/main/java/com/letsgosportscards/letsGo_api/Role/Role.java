a276ac02e3ddf245a088ed68d4f9df649093fa86
Alice Boone <alicegbone@gmail.com>
2021-02-05 15:31:36 -0800

    add ap. properties to gitignore

--- src/main/java/com/letsgosportscards/letsGo_api/Role/Role.java
+++ src/main/java/com/letsgosportscards/letsGo_api/Role/Role.java
@@ -1,51 +1,42 @@
 package com.letsgosportscards.letsGo_api.Role;
 
 import com.letsgosportscards.letsGo_api.User.User;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
+@Setter
+@Getter
+@ToString
 @Entity
 @Table
 public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(
+            name = "role_sequence",
+            sequenceName = "role_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Long id;
     private String name;
 
     @OneToMany(cascade= CascadeType.ALL)
-    private List<User> users;
+    List<User> users = new ArrayList<>();
 
-    public Role(long l, String admin) {
+    public Role() {
     }
 
     public Role(String name) {
         this.id = id;
         this.name = name;
-        this.users = users;
     }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }
 }
